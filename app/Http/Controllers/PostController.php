24313232fdac6674de439cc779d2b0d1cd00aa07
chounaikai <timpanist999@gmail.com>
2022-11-07 18:35:15 +0900

    08-04comp

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,21 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Request\PostReqest;
 use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(5)]);
     }
     public function show(Post $post)
     {
-        dd($post);
+        return view('posts/show')->with(['post' => $post]);
     }
     
     public function create()
     {
         return view('posts/create');
+    }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' =>$post]);
     }
 }
